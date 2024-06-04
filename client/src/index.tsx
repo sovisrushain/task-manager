8ca1e2ad8197b13c299ad7ea0398f6c71aa926a5
sovisrushain <sovisrushain@gmail.com>
2024-06-04 09:56:35 +0530

    fix react router render to create root compatible with react 18

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 import App from './App';
 import './index.css';
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
+reportWebVitals();
